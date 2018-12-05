99404486b2ba17dfad535676a0694c0228b0b165
sunfudan <lingganyang@gmail.com>
2018-12-05 23:49:28 +0800

    Create bst1.c
    实现一个简单的二叉搜索树，以及前序遍历

---
+++ binary_tree/binary_search_tree1/bst1.c
@@ -0,0 +1,66 @@
+/**
+ * @author      : sun (lingganyang@gmail.com)
+ * @file        : bst1
+ * @created     : Wednesday Dec 05, 2018 23:30:45 CST
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+typedef int DataType;
+typedef struct BinarySearchTreeNode{
+        DataType data;
+        struct BinarySearchTreeNode *left;
+        struct BinarySearchTreeNode *right;
+}BinarySearchTreeNode, *BinarySearchTreeNodePtr;
+
+
+BinarySearchTreeNodePtr create_node(DataType data){
+        BinarySearchTreeNodePtr node = (BinarySearchTreeNodePtr)malloc(sizeof(BinarySearchTreeNode));
+        if(node == NULL){
+                puts("Memory allocation failed");
+                return NULL;
+        }else{
+                node->data = data;
+                node->left = NULL;
+                node->right = NULL;
+        }
+
+        return node;
+}
+
+BinarySearchTreeNodePtr insert_node(BinarySearchTreeNodePtr root, DataType data){
+        if(root == NULL){
+                root = create_node(data);
+        }else if(root->data > data){
+                root->left = insert_node(root->left, data);
+        }else{
+                root->right = insert_node(root->right, data);
+        }
+        return root;
+}
+
+void preorder_visit(BinarySearchTreeNodePtr bst){
+        if(bst != NULL){
+                printf("%d\n", bst->data);
+                preorder_visit(bst->left);
+                preorder_visit(bst->right);
+        }
+}
+
+
+int main(void){
+        BinarySearchTreeNodePtr bst = NULL;
+
+        bst = insert_node(bst, 10);
+        bst = insert_node(bst, 3);
+        bst = insert_node(bst, 9);
+        bst = insert_node(bst, 11);
+        bst = insert_node(bst, 6);
+        
+        puts("pre order visit");
+        preorder_visit(bst);
+
+        return 0;
+}
